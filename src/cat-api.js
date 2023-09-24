f91d6bd7d95a827f9ea37fd77efa1a405c32ef8c
okaisym <okaisim@gmail.com>
2023-09-25 00:08:52 +0300

    cat-api added

---
+++ src/cat-api.js
@@ -0,0 +1,28 @@
+
+import axios from "axios";
+axios.defaults.headers.common["x-api-key"] = "live_c9ZRS6OQJ3MsG4emC1TF1C8g5pyzy9tccIxNWW0LSSj8UQHEkquwJyzXZuBJcMh9";
+
+
+
+export function fetchBreeds() {
+    return axios.get(`https://api.thecatapi.com/v1/breeds`)
+        .then((response) => {
+        return response.data;
+        })
+            .catch((error) => {
+            console.log(error);
+    })
+};
+
+
+export function fetchCatByBreed(breedId) {
+    return axios.get(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
+    .then((response) => {
+        return response.data[0];
+    }
+    )
+.catch((error) => {
+    console.log(error);
+});   
+}
+    
--- src/index.js
+++ src/index.js
@@ -1,5 +1,43 @@
-// import axios from "axios";
 
-// axios.defaults.headers.common["x-api-key"] = "твій ключ";
+import { fetchBreeds, fetchCatByBreed } from "./cat-api";
 
-// live_c9ZRS6OQJ3MsG4emC1TF1C8g5pyzy9tccIxNWW0LSSj8UQHEkquwJyzXZuBJcMh9
+const selector = document.querySelector('.breed-select');
+const loaderMessage = document.querySelector('.loader');
+const errorMessage = document.querySelector('.error');
+const divCat = document.querySelector('.cat-info');
+
+fetchBreeds()
+.then(breeds => {
+    const catInfo = breeds.map(({id, name}) => {
+        return {value: id, text: name}
+    })
+    catInfo.unshift({value: "", text: 'Select a breed', disabled: true})
+})
+.catch(error =>
+    console.log(error));
+
+selector.addEventListener('change', onSelect)
+
+function onSelect(event) {
+    const selectedBreedId = event.target.value;
+    if (breedId === "") {
+        return;
+      }
+
+    fetchCatByBreed(breedId) 
+    .then(breedInfo => {
+        const breedTextContent = breedInfo.breeds[0];
+    divCat.innerHTML = `<div class="cat-info">
+    <img src="${breedInfo.url}" alt="${breedTextContent.name}" width=800, height=450/>
+    <h2>${breedTextContent.name}</h2>
+    <p>${breedTextContent.description}</p>
+    <p>${breedTextContent.temperament}</p>
+          </div>`
+        }
+)
+.catch(error => 
+    console.log(error));
+};
+
+
+    
